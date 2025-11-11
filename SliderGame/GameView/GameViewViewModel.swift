0ba5503915b414dfea8930b8f4a36c610f189dc7
LeonidPirogov <lv.pirogof@gmail.com>
2025-11-11 20:22:03 +0300

    feat: initial version of SliderGame with slider logic and UI

---
+++ SliderGame/GameView/GameViewViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  GameViewViewModel.swift
+//  SliderGame
+//
+//  Created by Leonid on 11.11.2025.
+//
+
+import Foundation
---
+++ SliderGame/Models/GameOptions.swift
@@ -0,0 +1,8 @@
+//
+//  GameOptions.swift
+//  SliderGame
+//
+//  Created by Leonid on 11.11.2025.
+//
+
+import Foundation
