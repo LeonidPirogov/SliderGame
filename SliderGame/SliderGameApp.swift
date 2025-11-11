3021a1339e356a35b6e6cfbb7b36ee85d35f7afa
LeonidPirogov <lv.pirogof@gmail.com>
2025-11-11 11:03:32 +0300

    Initial Commit

---
+++ SliderGame/SliderGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderGameApp.swift
+//  SliderGame
+//
+//  Created by Leonid on 11.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct SliderGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
